56c9a11e153fc59d78ca4ca9b375a4ca0982b912
Ravindra Vithanage <ravindra.vithanage@cenium.com>
2022-06-20 11:00:43 +0530

    RnD repo initial test commit

---
+++ RnD/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RnD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial");
+            FncInitial();
+        }
+
+        static void FncInitial()
+        {
+            Console.WriteLine("Initial");
+        }
+    }
+}
